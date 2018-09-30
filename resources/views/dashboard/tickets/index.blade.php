471b3ee6f97a15286bb99b23419583ffaad38fda
bashari <bashari.mwd@gmail.com>
2018-09-30 15:07:57 +0330

    Add temp ticket

---
+++ resources/views/dashboard/tickets/index.blade.php
@@ -0,0 +1,13 @@
+@extends('dashboard.master')
+
+@section('title')
+    تیکت ها
+@endsection
+
+@section('content')
+    <h1>تیکت ها</h1>
+@endsection
+
+@section('js-footer')
+
+@endsection
---
+++ resources/views/dashboard/tickets/show.blade.php
@@ -0,0 +1,13 @@
+@extends('dashboard.master')
+
+@section('title')
+    جزئیات تیکت
+@endsection
+
+@section('content')
+    <h1>جزئیات تیکت</h1>
+@endsection
+
+@section('js-footer')
+
+@endsection
