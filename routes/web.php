9dbd7bb29ea920c9fca8ba322ed7a40b6e5feb24
bashari <bashari.mwd@gmail.com>
2018-10-15 12:31:59 +0330

    Add user panel page

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,10 @@ Route::get('/', 'PageController@home')->name('home');
 
 //************************************* auth *******************************************
 Auth::routes();
+
+//************************************ user panel **************************************
+Route::group(['prefix' => '/panel', 'as' => 'panel.'], function () {
+    Route::get('/', function (){
+        return view('panel.home');
+    })->name('home');
+});
